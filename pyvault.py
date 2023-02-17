56a3eff197e8ba2afd0552e70079008a8dc3180a
EResplandes <resplandes930@gmail.com>
2023-02-16 22:31:33 -0300

    Inserção do modúlo de atualização de informações de sistemas

--- pyvault.py
+++ pyvault.py
@@ -18,6 +18,7 @@ cursor = conexao.cursor()
 data_atual = date.today()
 
 # Cabeçalho do Sistema
+os.system('cls' if os.name == 'nt' else 'clear')
 print('---------------------------------------------------------------------------------')
 print('-------------------- Sistema de Controle de Senhas - PyVault --------------------')
 print('---------------------------------------------------------------------------------')
@@ -50,14 +51,24 @@ if usuario == user_formatado:
         print(f'Seja Bem-vindo, {usuario}!')
         print('--------------------------------------------------------------------------------')
 
+        query_busca_tipo_usuario = f'SELECT tipo_usuario FROM tb_usuarios WHERE nome_usuario = "{usuario}"'
+        cursor.execute(query_busca_tipo_usuario)
+        query_busca_tipo_usuario_resultado = cursor.fetchall()
+        query_busca_tipo_usuario_resultado_formatado = str(query_busca_tipo_usuario_resultado)[3:-4]
 
+    
         print('Menu Principal')
         print('1) Cadastrar novo sistema')
         print('2) Verificar informações de sistema')
-        print('3) Atualizar informação de sistema')
+        print('3) Atualizar informação de sistema')   
+        print('4) Exclusão de sistema')
+        print('5) Cadastro de Usuário')
+        print('6) Exclusão de Usuário')
         opcao_selecionada = input('Selecione umas das opções:')
+        
+
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        #Modúlo de Cadastro de Sistema
+        #Modúlo de Cadastro de Sistema''
         if (opcao_selecionada == '1'):
             os.system('cls' if os.name == 'nt' else 'clear')
             #Formúlario
@@ -175,11 +186,184 @@ if usuario == user_formatado:
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
          #Modúlo de Atualização de Sistema
         elif (opcao_selecionada == '3'):
-             print('atualizar')
+            print('--------------------------------------------------------------------------------------------------------------------')
+            print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+            print('--------------------------------------------------------------------------------------------------------------------')
+
+            print('1) Buscar pelo nome:  ')
+            print('2) Buscar pelo Ip: ')
+            guarda_opcao_a = input('Selecione o parametrô de busca:')
+
+            if (guarda_opcao_a == '1'):
+                 os.system('cls' if os.name == 'nt' else 'clear')
+                 print('--------------------------------------------------------------------------------------------------------------------')
+                 print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                 print('--------------------------------------------------------------------------------------------------------------------')
+                 pede_nome = input('Insira o nome do sistema que deseja atualizar a informação: ')
+                 qyery_busca_sistema_nome = f'SELECT nome_sistema FROM tb_sistemas WHERE nome_sistema LIKE "%{pede_nome}%"'
+                 cursor.execute(qyery_busca_sistema_nome)
+                 qyery_busca_sistema_nome_resultado = cursor.fetchall()
+                 qyery_busca_sistema_nome_resultado_formatado = str(qyery_busca_sistema_nome_resultado)[3:-4]
+
+                 os.system('cls' if os.name == 'nt' else 'clear')
+                 print('--------------------------------------------------------------------------------------------------------------------')
+                 print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                 print('--------------------------------------------------------------------------------------------------------------------')
+
+                 print('1) Atualizar nome do sistema: ')
+                 print('2) Atualizar ip do sistema: ')
+                 print('3) Atualizar usuário do sistema: ')
+                 print('4) Atualizar senha do sistema: ')
+                 guarda_parametro_atualizacao = input('Selecione o parametrô que deseja atualizar: ')
+                
+                 if (guarda_parametro_atualizacao == '1'):
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    guarda_parametro_atualizacao_novo_nome = input('Informe o novo nome do sistema: ') 
+
+                    atualiza_nome = f'UPDATE tb_sistemas SET nome_sistema = "{guarda_parametro_atualizacao_novo_nome}" WHERE  nome_sistema = "{qyery_busca_sistema_nome_resultado_formatado}"'
+                    cursor.execute(atualiza_nome)
+                    conexao.commit()
+
+                    # query_confirma_atualizacao_nome = f'SELECT nome_sistema FROM tb_sistemas WHERE nome_sistema = "{guarda_parametro_atualizacao_novo_nome}"'
+                    # cursor.execute(query_confirma_atualizacao_nome)
+                    # query_confirma_atualizacao_nome_resultado = cursor.fetchall()
+                    # query_confirma_atualizacao_nome_resultado_formatado = str(qyery_busca_sistema_nome_resultado)[3:-4]
+                    
+                    print('Atualização realizada com sucesso!')
+
+                 elif (guarda_parametro_atualizacao == '2'):
+                    
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    guarda_parametro_atualizacao_novo_ip = input('Informe o novo ip do sistema: ')
+
+                    atualiza_ip = f'UPDATE tb_sistemas SET ip_sistema = "{guarda_parametro_atualizacao_novo_ip}" WHERE nome_sistema = "{qyery_busca_sistema_nome_resultado_formatado}"'
+                    cursor.execute(atualiza_ip)
+                    conexao.commit()
+
+                    print('Atualização realizada com sucesso!')
+
+                 elif (guarda_parametro_atualizacao == '3'):
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    guarda_parametro_atualizacao_novo_usuario = input('Informe o novo nome do usuário sistema: ')
+
+                    atualiza_usuario = f'UPDATE tb_sistemas SET usuario_sistema = "{guarda_parametro_atualizacao_novo_usuario}" WHERE nome_sistema = "{qyery_busca_sistema_nome_resultado_formatado}"'
+                    cursor.execute(atualiza_usuario)
+                    conexao.commit()
+
+                    print('Atualização realizada com sucesso!')
+
+                 elif (guarda_parametro_atualizacao == '4'):
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    guarda_parametro_atualizacao_nova_senha = input('Informe a nova senha sistema: ')
+                    
+                    atualiza_senha = f'UPDATE tb_sistemas SET senha_sistema = "{guarda_parametro_atualizacao_nova_senha}" WHERE nome_sistema = "{qyery_busca_sistema_nome_resultado_formatado}"'
+                    cursor.execute(atualiza_senha)
+                    conexao.commit()
+
+                 else:
+                    print('Parametrô inválido!')
+                   
+
+            elif (guarda_opcao_a == '2'):
+                 os.system('cls' if os.name == 'nt' else 'clear')
+                 print('--------------------------------------------------------------------------------------------------------------------')
+                 print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                 print('--------------------------------------------------------------------------------------------------------------------')
+                 pede_ip = input('Insira o ip do sistema que deseja atualizar a informação: ')
+                 qyery_busca_sistema_ip = f'SELECT ip_sistema FROM tb_sistemas WHERE ip_sistema = "{pede_ip}"'
+                 cursor.execute(qyery_busca_sistema_ip)
+                 qyery_busca_sistema_ip_resultado = cursor.fetchall()
+                 qyery_busca_sistema_ip_resultado_formatado = str(qyery_busca_sistema_ip_resultado)[3:-4]
+
+                 os.system('cls' if os.name == 'nt' else 'clear')
+                 print('--------------------------------------------------------------------------------------------------------------------')
+                 print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                 print('--------------------------------------------------------------------------------------------------------------------')
+
+                 print('1) Atualizar nome do sistema: ')
+                 print('2) Atualizar ip do sistema: ')
+                 print('3) Atualizar usuário do sistema: ')
+                 print('4) Atualizar senha do sistema: ')
+                 guarda_parametro_atualizacao = input('Selecione o parametrô que deseja atualizar: ')
+                
+                 if (guarda_parametro_atualizacao == '1'):
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    guarda_parametro_atualizacao_novo_nome = input('Informe o novo nome do sistema: ') 
+
+                    atualiza_nome = f'UPDATE tb_sistemas SET nome_sistema = "{guarda_parametro_atualizacao_novo_nome}" WHERE  ip_sistema = "{qyery_busca_sistema_ip_resultado_formatado}"'
+                    cursor.execute(atualiza_nome)
+                    conexao.commit()
+
+                    # query_confirma_atualizacao_nome = f'SELECT nome_sistema FROM tb_sistemas WHERE nome_sistema = "{guarda_parametro_atualizacao_novo_nome}"'
+                    # cursor.execute(query_confirma_atualizacao_nome)
+                    # query_confirma_atualizacao_nome_resultado = cursor.fetchall()
+                    # query_confirma_atualizacao_nome_resultado_formatado = str(qyery_busca_sistema_nome_resultado)[3:-4]
+                    
+                    print('Atualização realizada com sucesso!')
+
+                 elif (guarda_parametro_atualizacao == '2'):
+                    
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    guarda_parametro_atualizacao_novo_ip = input('Informe o novo ip do sistema: ')
+
+                    atualiza_ip = f'UPDATE tb_sistemas SET ip_sistema = "{guarda_parametro_atualizacao_novo_ip}" WHERE ip_sistema = "{qyery_busca_sistema_ip_resultado_formatado}"'
+                    cursor.execute(atualiza_ip)
+                    conexao.commit()
+
+                    print('Atualização realizada com sucesso!')
+
+                 elif (guarda_parametro_atualizacao == '3'):
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    guarda_parametro_atualizacao_novo_usuario = input('Informe o novo nome do usuário sistema: ')
+
+                    atualiza_usuario = f'UPDATE tb_sistemas SET usuario_sistema = "{guarda_parametro_atualizacao_novo_usuario}" WHERE ip_sistema = "{qyery_busca_sistema_ip_resultado_formatado}"'
+                    cursor.execute(atualiza_usuario)
+                    conexao.commit()
+
+                    print('Atualização realizada com sucesso!')
+
+                 elif (guarda_parametro_atualizacao == '4'):
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    print('------------------------------------ Modúlo de Atualização de Informações ------------------------------------------')
+                    print('--------------------------------------------------------------------------------------------------------------------')
+                    guarda_parametro_atualizacao_nova_senha = input('Informe a nova senha sistema: ')
+                    
+                    atualiza_senha = f'UPDATE tb_sistemas SET senha_sistema = "{guarda_parametro_atualizacao_nova_senha}" WHERE ip_sistema = "{qyery_busca_sistema_ip_resultado_formatado}"'
+                    cursor.execute(atualiza_senha)
+                    conexao.commit()
+
+                 else:
+                    print('Parametrô inválido!')
+                   
+            else:
+                print('Parametrô inválido!')
+
+# Fim do Modúlo de Atualização de Sistema
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Início do Modúlo de Exclusão de Sistema - Administrador
         elif (opcao_selecionada == '4'):
+             #Modúlo de Exclusão de sistema
              print('sei la')
         else:
-            print('Atualizar')
+            print('Opção inválida!')
 
     else:
         print('Senha incorreta!')
