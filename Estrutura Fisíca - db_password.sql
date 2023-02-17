1523f2f6bd4acbcfa5a8e295033777a8b140d56f
EResplandes <resplandes930@gmail.com>
2023-02-16 22:33:35 -0300

    Inclusão da tabela tipo_sistema

--- Estrutura Fisíca - db_password.sql
+++ Estrutura Fisíca - db_password.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS tb_usuarios (
 	id_usuario INT(11) AUTO_INCREMENT,
     nome_usuario VARCHAR(50) NOT NULL,
     senha_usuario BLOB(50) NOT NULL,
+    tipo_usuario ENUM('Administrador', 'Usuário') NOT NULL,
 PRIMARY KEY (id_usuario)
 )AUTO_INCREMENT = 1;
 
@@ -13,5 +14,13 @@ CREATE TABLE IF NOT EXISTS tb_sistemas (
     ip_sistema VARCHAR(50) NOT NULL,
     usuario_sistema VARCHAR(50) NOT NULL,
     senha_sistema BLOB NOT NULL,
-PRIMARY KEY (id_sistema)
+    id_tipo_sistema INT(11) NOT NULL,
+PRIMARY KEY (id_sistema),
+CONSTRAINT fk_id_tipo_sistema FOREIGN KEY(id_tipo_sistema) REFERENCES tb_tipo_sistema (id_tipo_sistema)
 )AUTO_INCREMENT = 1;
+
+CREATE TABLE IF NOT EXISTS tb_tipo_sistema (
+	id_tipo_sistema INT(11) AUTO_INCREMENT,
+    nome_tipo_sistema VARCHAR(50) NOT NULL,
+PRIMARY KEY (id_tipo_sistema)
+);
